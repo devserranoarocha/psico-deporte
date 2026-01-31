1f257d7101a8db0a9cd64d7d22c3af08293e644d
Salvador Serrano <serranoarocha@gmail.com>
2026-01-31 13:42:34 +0100

    news completed - endpoints comprobed (#3)

---
+++ symfony-backend/src/Entity/News.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\NewsRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: NewsRepository::class)]
+class News
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTime $date = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $news_text = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getDate(): ?\DateTime
+    {
+        return $this->date;
+    }
+
+    public function setDate(\DateTime $date): static
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    public function getNewsText(): ?string
+    {
+        return $this->news_text;
+    }
+
+    public function setNewsText(string $news_text): static
+    {
+        $this->news_text = $news_text;
+
+        return $this;
+    }
+}
