5ae3aed7abb92c0466a2679e46de8a1407079f94
Salvador Serrano <serranoarocha@gmail.com>
2026-02-04 09:04:43 +0100

    navigation to init fixed (#9)

--- angular-frontend/src/app/app.config.ts
+++ angular-frontend/src/app/app.config.ts
@@ -1,12 +1,17 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { ApplicationConfig, provideZoneChangeDetection} from '@angular/core';
+import { provideRouter, withInMemoryScrolling  } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }),
-              provideRouter(routes),
-              provideHttpClient()
+              provideRouter(routes, 
+                            withInMemoryScrolling({ scrollPositionRestoration: 'enabled', // Vuelve arriba al navegar
+                                                    anchorScrolling: 'enabled'            // Permite ir a #ids
+                                                  })
+                            ),
+              provideHttpClient(),
+              
             ]
 };
